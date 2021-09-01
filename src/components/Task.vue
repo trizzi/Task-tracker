9f29de228240de8041357695e559719ca5d6ba1a
trizzi <patrickakhamiogu@gmail.com>
2021-09-01 18:37:37 +0100

    updated reminder features...

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,7 +1,13 @@
 <template>
-  <h3>
-    {{ task.text }}
-  </h3>
+  <div @dblclick="$emit('toggle-reminder', task.id)" :class="[task.reminder ? 'reminder' : '', 'task']">
+    <h3>
+      {{ task.text }}
+      <i @click='onDelete()' class="fas fa-times"></i>
+    </h3>
+    <p>
+      {{ task.day }}
+    </p>
+  </div>
 </template>
 
 <script>
@@ -10,5 +16,30 @@ export default {
   props: {
     task: Object,
   },
+  methods:{
+    onDelete(id){
+      this.$emit('delete-task', id)
+    }
+  }
 };
 </script>
+
+<style scoped>
+.task{
+  background: #f4f4f4;
+  margin: 5px;
+  padding: 10px 20px;
+  cursor: pointer;
+}
+.task.reminder{
+  border-left: 5px solid green;  
+}
+.task h3{
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.fas{
+  color: red;
+}
+</style>
