ec511a850b6a87eb47a3a001d757e60811bb4f30
trizzi <patrickakhamiogu@gmail.com>
2021-09-05 19:21:26 +0100

    Added delete and reminder functionalities

--- src/App.vue
+++ src/App.vue
@@ -41,13 +41,29 @@ export default {
       const data = await res.json()
          this.tasks = [...this.tasks, data]
     },
-    deleteTask(id){
+   async deleteTask(id){
       if(confirm('Are you sure?')){
-            this.tasks = this.tasks.filter((task) => task.id !== id)
+          const res = await fetch(`api/tasks/${id}`, {
+            method: 'DELETE'
+          })
+          res.status === 200 ? (this.tasks = this.tasks.filter((task) => task.id !== id)) : alert('Issue with deleting task')
       } 
     },
-    toggleReminder(id){
-      this.tasks = this.tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task)
+   async toggleReminder(id){
+     const taskToToggle = await this.fetchData(id)
+     const updateTask = {...taskToToggle, reminder: !taskToToggle.reminder}
+     
+     const res = await fetch(`api/tasks/${id}`, {
+       method: 'PUT',
+        headers:{
+          'Content-type': 'application/json',
+        },
+         body: JSON.stringify(updateTask)
+     })
+     
+       const data = await res.json()
+
+      this.tasks = this.tasks.map((task) => task.id === id ? {...task, reminder: data.reminder} : task)
     },
     async fetchData(){
       const res = await fetch('api/tasks')
